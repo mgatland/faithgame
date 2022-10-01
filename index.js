8f6382cfb1c89e939f1c64f33c49f87fc8d89343
Matthew Gatland <matthew.gatland@mojang.com>
2022-10-02 01:11:33 +0200

    trying again

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ app.use("/", express.static(__dirname + '/'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-console.log("Listening at port " + port)
-app.listen(port);
+console.log("Trying to listen at port " + port)
+app.listen(port, () => console.log('listening on port ' + port))
 
 app.get('/test', function(req, res) {
 	res.send('ok')
 })
+
+app.get("/", (req, res) => res.type('html').send(html));
+const html = `hello`
