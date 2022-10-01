8f8a21043c5876aa2887bf21a27050b77765a673
Matthew Gatland <matthew.gatland@mojang.com>
2022-10-02 01:37:39 +0200

    desperate testing

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-"use strict";
-
-// process.title = 'faithgame';
-var express = require("express");
-// var bodyParser = require('body-parser')
-var app = express();
-var port = process.env.PORT || 8080;
-
-// console.log('dir ' + __dirname)
-// app.use("/", express.static(__dirname + '/'));
-
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-console.log("Trying to listen at port " + port)
-app.listen(port, () => console.log('listening on port ' + port))
-
-app.get('/test', function(req, res) {
-	res.send('ok')
-})
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
-const html = `<html><head></head><body>hi</body></html>`
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
